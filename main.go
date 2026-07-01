94bfd00d6448ff1e924ddd8a99b8ca3614b636f0
jmiguel-hdez <jmiguel.hdez@gmail.com>
2026-06-30 22:44:33 -0500

    split to commands.go

--- main.go
+++ main.go
@@ -1,55 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/jmiguel-hdez/bootdev-blogaggregator-go/internal/config"
 	"log"
 	"os"
 )
-
-type state struct {
-	cfg *config.Config
-}
-
-type command struct {
-	name string
-	args []string
-}
-
-type commands struct {
-	cmds map[string]func(*state, command) error
-}
-
-// This method runs a given command with the provided state if it exists
-func (c *commands) run(s *state, cmd command) error {
-	f, ok := c.cmds[cmd.name]
-	if !ok {
-		return fmt.Errorf("%s is not a registered command", cmd.name)
-	}
-	err := f(s, cmd)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// This method registers a new handler function for a command name
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.cmds[name] = f
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) != 1 {
-		return fmt.Errorf("Login expect one argument <username>")
-	}
-	username := cmd.args[0]
-	err := s.cfg.SetUser(username)
-	if err != nil {
-		return fmt.Errorf("unable to set username")
-	}
-	fmt.Printf("User has been set to %s\n", username)
-	return nil
-}
 
 func main() {
 	cfg, err := config.Read()
