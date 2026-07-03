269c8d42b0804ebcba6d51c866c49ee95a3e7ceb
jmiguel-hdez <jmiguel.hdez@gmail.com>
2026-07-03 16:01:56 -0500

    clean up

--- handler_agg.go
+++ handler_agg.go
@@ -2,75 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/xml"
 	"fmt"
-	"html"
-	"io"
-	"net/http"
 )
-
-type RSSItem struct {
-	Title       string `xml:"title"`
-	Link        string `xml:"link"`
-	Description string `xml:"description"`
-	PubDate     string `xml:"pubDate"`
-}
-
-type RSSFeed struct {
-	Channel struct {
-		Title       string    `xml:"title"`
-		Link        string    `xml:"link"`
-		Description string    `xml:"description"`
-		Item        []RSSItem `xml:"item"`
-	} `xml:"channel"`
-}
-
-func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
-
-	var feed RSSFeed
-
-	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
-	if err != nil {
-		return &RSSFeed{}, fmt.Errorf("unable to create new request: %w", err)
-	}
-
-	req.Header.Set("User-Agent", "gator")
-
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return &RSSFeed{}, fmt.Errorf("request error: %w", err)
-	}
-	defer resp.Body.Close()
-
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return &RSSFeed{}, fmt.Errorf("error reading response: %w", err)
-	}
-
-	err = xml.Unmarshal(data, &feed)
-
-	if err != nil {
-		return &RSSFeed{}, fmt.Errorf("error decoding feed %w", err)
-	}
-
-	// Use the html.UnescapeString function to decode escaped HTML entities. You'll need to run the Title and Description fields
-	// (of both the entire channel as well as the items.) through this function
-	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
-	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
-	for i, item := range feed.Channel.Item {
-		feed.Channel.Item[i].Title = html.UnescapeString(item.Title)
-		feed.Channel.Item[i].Description = html.UnescapeString(item.Description)
-	}
-
-	return &feed, nil
-}
 
 func handleAgg(s *state, cmd command) error {
 	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
 	if err != nil {
 		return fmt.Errorf("unable to fetch feed: %w", err)
 	}
-	fmt.Printf("%+v\n", *feed)
+	fmt.Printf("Feed: %+v\n", feed)
 	return nil
 }
